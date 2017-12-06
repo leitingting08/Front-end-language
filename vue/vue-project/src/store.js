006dcc1b0968be8e8883f7d6790ef69aba8b405c
leitingting08 <leitingting08@gmail.com>
2017-12-06 23:32:21 +0800

    delete vue-project

--- vue/vue-project/src/store.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = 'todos-vuejs'
-export default {
-	fetch:function () {
-		return JSON.parse(window.localStorge.getItem(STORAGE_KEY) || '[]')
-	},
-	save:function (items) {
-		window.localStorge.setItem(STORAGE_KEY, JSON.stringify(items))
-	}
-}
